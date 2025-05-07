927e1856286b32b3e424551e352521aeb0e3de0d
marleni <marleni_angel@brown.edu>
2025-05-07 17:24:26 -0400

    eval script for simclr

--- code/models/simclr_eval.py
+++ code/models/simclr_eval.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 from torchvision.models import resnet18
 
 from pytorch_lightning import seed_everything
-from models.simclr import SimCLR  # Adjust this import path if needed
+from code.models.simclr import SimCLR  
 
 # Load trained encoder
 CHECKPOINT_PATH = 'lightning_logs/version_0/checkpoints'
