24a2031e3ca846818715d156b608b59f1bfac43e
Melyssa Cristino <melyssacristino@Melyssas-MacBook-Air.local>
2025-05-07 22:03:31 -0400

    removed normalization

--- code/models/clip.py
+++ code/models/clip.py
@@ -28,9 +28,7 @@ class CLIPClassifier(nn.Module):
     def transform(self):
         return transforms.Compose([
             transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073],
-                                 std=[0.26862954, 0.26130258, 0.27577711])
+            transforms.ToTensor()
         ])
 
 def collate_fn(batch, processor):
